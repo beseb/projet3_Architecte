cdd53944f18ce5363e63002be03269e64e70be4a
[Sebastien Escudero] <sebastien.escudero@gmail.com>
2023-07-21 02:07:39 +0200

    added Ajout photo dialog

--- FrontEnd/assets/dialog.js
+++ FrontEnd/assets/dialog.js
@@ -1,26 +1,27 @@
 import { travaux } from "./script.js";
 import { genererAffichageTravaux } from "./gallery.js";
- 
+import { genererCategories } from "./gallery.js";
 
-
-const overlay = document.querySelector(".overlay")
-let modal1 = document.querySelector(".modal1")
-
+const overlay = document.querySelector(".overlay");
+let modal1 = document.querySelector(".modal1");
+let modal2 = document.querySelector(".modal2");
+/**
+ * Fonction qui génerer l'affichage de la modal 1
+ */
 export function affichageModal() {
-    
-    // Vide l'interieur de la modal
-    if (modal1) {modal1.innerHTML = ""}
-    overlay.style.display = "block";
-    modal1.style.display="flex";
-    modal1.removeAttribute("aria-hidden");
-    modal1.setAttribute("aria-modal", "true");
-    // Ferme la modal si l'on clique en dehors
-   // modal1.addEventListener("click", closeModal);
-  
-    modal1.innerHTML = `
-    <div class = "modal1-wrapper">
+  // Vide l'interieur de la modal
+  if (modal1) {
+    modal1.innerHTML = "";
+  }
+  overlay.style.display = "block";
+  modal1.style.display = "flex";
+  modal1.removeAttribute("aria-hidden");
+  modal1.setAttribute("aria-modal", "true");
+
+  modal1.innerHTML = `
+    <div class = "modal-wrapper">
     <div class="divBoutonFermer">
-        <button class="modal1-bouton-fermer"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+        <button class="modal-bouton-fermer"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
         <path d="M11.6546 2.05106C12.1235 1.58214 12.1235 0.820611 11.6546 0.351691C11.1856 -0.11723 10.4241 -0.11723 9.95519 0.351691L6.005 4.30563L2.05106 0.355442C1.58214 -0.113479 0.820611 -0.113479 0.351691 0.355442C-0.11723 0.824363 -0.11723 1.58589 0.351691 2.05481L4.30563 6.005L0.355442 9.95894C-0.113479 10.4279 -0.113479 11.1894 0.355442 11.6583C0.824363 12.1272 1.58589 12.1272 2.05481 11.6583L6.005 7.70437L9.95894 11.6546C10.4279 12.1235 11.1894 12.1235 11.6583 11.6546C12.1272 11.1856 12.1272 10.4241 11.6583 9.95519L7.70437 6.005L11.6546 2.05106Z" fill="black"/>
           </svg></button>
     </div>
@@ -30,34 +31,142 @@ export function affichageModal() {
     <hr>
     <button class="modal1-ajouter-une-photo">Ajouter une photo</button>
     <button class="modal1-supprimer-toutes-photos">Supprimer la galerie</button>
-    </div>`
-    ;
-    genererAffichageTravaux(travaux,"modal")
-    ajouterEvenements();
- }
-/**
- * Fonction pour fermer la modale
- * @param {} e 
- */
- const closeModal = () => {
-     window.setTimeout(function () {
-         modal1.style.display = "none";
-         overlay.style.display = null;
-    
-    }, 200)
-    modal1.setAttribute("aria-hidden", "true");
-    modal1.removeAttribute("aria-modal");
-   /* modal1.removeEventListener("click", closeModal);
-    modal1
-      .querySelector(".js-modal-close")
-      .removeEventListener("click", closeModal);*/
-  
-  };
-function ajouterEvenements(){
-    const boutonFermer = document.querySelector(".modal1-bouton-fermer")
-    if(boutonFermer){
-        boutonFermer.addEventListener("click",() => {
-            closeModal();
-        })
-    }
+    </div>`;
+  genererAffichageTravaux(travaux, "modal");
+  ajouterEvenementsBoutons();
+
+  /**
+   * EventListener pour ouvrir la modale 2
+   */
+  const ajouterPhoto = document.querySelector(".modal1-ajouter-une-photo");
+  console.log(ajouterPhoto);
+  if (ajouterPhoto) {
+    ajouterPhoto.addEventListener("click", () => {
+      affichageModal2();
+    });
+  }
 }
+
+/**
+ * Fonction pour générer l'affichage de la modal 2
+ */
+export function affichageModal2() {
+  modal2.style.display = "flex";
+  modal2.removeAttribute("aria-hidden");
+  modal2.setAttribute("aria-modal", "true");
+
+  modal2.innerHTML = `
+  <div class = "modal-wrapper" id="modal2">
+  <div class="divBoutonFermer">
+      <button class="modal-bouton-fermer2"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+      <path d="M11.6546 2.05106C12.1235 1.58214 12.1235 0.820611 11.6546 0.351691C11.1856 -0.11723 10.4241 -0.11723 9.95519 0.351691L6.005 4.30563L2.05106 0.355442C1.58214 -0.113479 0.820611 -0.113479 0.351691 0.355442C-0.11723 0.824363 -0.11723 1.58589 0.351691 2.05481L4.30563 6.005L0.355442 9.95894C-0.113479 10.4279 -0.113479 11.1894 0.355442 11.6583C0.824363 12.1272 1.58589 12.1272 2.05481 11.6583L6.005 7.70437L9.95894 11.6546C10.4279 12.1235 11.1894 12.1235 11.6583 11.6546C12.1272 11.1856 12.1272 10.4241 11.6583 9.95519L7.70437 6.005L11.6546 2.05106Z" fill="black"/>
+        </svg></button>
+  </div>
+  <div class="divBoutonRetour"><button class="modal-bouton-retour"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="19" viewBox="0 0 21 19" fill="none">
+  <path d="M0.439478 7.94458C-0.146493 8.53055 -0.146493 9.48217 0.439478 10.0681L7.9399 17.5686C8.52587 18.1545 9.47748 18.1545 10.0635 17.5686C10.6494 16.9826 10.6494 16.031 10.0635 15.445L5.11786 10.5041H19.4999C20.3297 10.5041 21 9.83375 21 9.00402C21 8.17428 20.3297 7.50393 19.4999 7.50393H5.12255L10.0588 2.56303C10.6447 1.97706 10.6447 1.02545 10.0588 0.439478C9.47279 -0.146493 8.52118 -0.146493 7.93521 0.439478L0.43479 7.9399L0.439478 7.94458Z" fill="black"/>
+</svg></button>
+  </div>
+  <p class="modal2-title">Ajout Photo</p>
+  <div class="divAjoutPhoto"><img src="/FrontEnd/assets/icons/picture-svgrepo-com-1.jpg" id="previewImg"></img><button class="upload-button"><input type="file" id="file" name="file" accept=".png, .jpg, .jpeg" max-size="4MB">+Ajouter photo</button><p>jpg, png: 4mo max</p></div>
+  
+  
+  <form class="formModal2" action="/upload" method="post" enctype="multipart/form-data" >
+  <label name="Titre">Titre</label>
+  <input type="text" name="Titre" id="Titre" required></input>
+  
+  <label for="categorie">Catégorie</label>
+  <select name="categorie" id="categorie">
+  </select>
+    <hr>
+    <button class="boutonValiderAjout" type ="submit">Valider</button>
+    </form>
+  </div>`;
+  genererCategories();
+  ajouterEvenementsBoutons();
+
+  /**
+   * Permettre de télécharger une image, en ayant le input[type="file"] caché, pour plus de design et la preview du document televersé
+   */
+
+  const fileInput = document.getElementById("file");
+  const uploadButton = document.querySelector(".upload-button");
+  // Cet addEventListener permet de cliquer virtuellement sur le input[type="file"]
+  uploadButton.addEventListener("click", () => {
+    fileInput.click();
+  });
+
+  const previewImg = document.getElementById("previewImg");
+
+  fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        previewImg.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  });
+}
+/**
+ * Fonction pour fermer les modales
+ *
+ */
+function closeModal() {
+  modal1.style.display = "none";
+  modal2.style.display = "none";
+  overlay.style.display = null;
+
+  modal1.setAttribute("aria-hidden", "true");
+  modal1.removeAttribute("aria-modal");
+  modal2.setAttribute("aria-hidden", "true");
+  modal2.removeAttribute("aria-modal");
+}
+/**
+ * Fonction pour fermer les modales
+ * @param {} e
+ */
+function closeModal2() {
+  modal2.style.display = "none";
+  modal2.setAttribute("aria-hidden", "true");
+  modal2.removeAttribute("aria-modal");
+}
+/**
+ * Fonction pour ajouter le bouton Fermer & Retour
+ */
+function ajouterEvenementsBoutons() {
+  const boutonFermer = document.querySelector(".modal-bouton-fermer");
+  const boutonFermer2 = document.querySelector(".modal-bouton-fermer2");
+
+  const boutonRetour = modal2.querySelector(".modal-bouton-retour");
+  if (boutonFermer) {
+    boutonFermer.addEventListener("click", () => {
+      closeModal();
+    });
+  }
+  if (boutonFermer2) {
+    boutonFermer2.addEventListener("click", () => {
+      closeModal();
+    });
+  }
+  if (boutonRetour) {
+    boutonRetour.addEventListener("click", () => {
+      closeModal2();
+    });
+  }
+}
+
+// Fonction pour gener la fermeture des modales lors d'un clic en dehors
+function handleClickOutsideModal(event) {
+  if (
+    (event.target === overlay && event.target !== modal1) ||
+    (event.target === document &&
+      event.target !== modal1 &&
+      event.target !== modal2)
+  ) {
+    closeModal();
+    closeModal2();
+  }
+}
+// eventListener pour écouter les clicks en dehors des modales
+document.addEventListener("click", handleClickOutsideModal);
--- FrontEnd/assets/gallery.js
+++ FrontEnd/assets/gallery.js
@@ -1,10 +1,8 @@
 /**
- *Fonction qui génère l'affiche des travaux avec {travaux} en paramètre
+ *Fonction qui génère l'affiche des travaux avec {travaux} en paramètre, en fonction de l'endroit de la page où elle doit être
  * */
-export async function genererAffichageTravaux(
-  travaux,
-  type = "type"
-) {
+export async function genererAffichageTravaux(travaux, type = "type") {
+  // si l'affichage doit se faire dans la gallery !
   if (type === "gallery") {
     // Récuperation de la div "Porfolio" et de la div "Gallery" si elle existe
     let container = document.getElementById("portfolio");
@@ -32,15 +30,23 @@ export async function genererAffichageTravaux(
     // Ajout de la divGallery à la section Portfolio
     container.appendChild(divGallery);
   }
+  // Si l'affichage doit se faire dans la modale !
   if (type === "modal") {
     let container = document.querySelector(".modal1-photos");
     let modal1PhotosWrapper = document.createElement("div");
-    modal1PhotosWrapper.className = "modal1-photos-wrapper"
+    modal1PhotosWrapper.className = "modal1-photos-wrapper";
     // Boucle pour créer les éléments dans la galerie
     for (let i = 0; i < travaux.length; i++) {
       let figGallery = document.createElement("figure");
       let figCaptionGallery = document.createElement("figcaption");
-      figGallery.innerHTML = `<img src = "${travaux[i].imageUrl}" alt = ${travaux[i].title}></img>`;
+      figGallery.innerHTML = `<img src = "${travaux[i].imageUrl}" alt = ${travaux[i].title}>
+        <button class = "movePhoto"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+        <rect width="17" height="17" rx="2" fill="black"/>
+        <path d="M9.05089 3.20363C8.77938 2.93212 8.33845 2.93212 8.06694 3.20363L6.6768 4.59377C6.40529 4.86528 6.40529 5.30621 6.6768 5.57772C6.94831 5.84924 7.38925 5.84924 7.66076 5.57772L7.86493 5.37355V7.86493H5.37355L5.57772 7.66076C5.84924 7.38925 5.84924 6.94831 5.57772 6.6768C5.30621 6.40529 4.86528 6.40529 4.59377 6.6768L3.20363 8.06694C2.93212 8.33845 2.93212 8.77938 3.20363 9.05089L4.59377 10.441C4.86528 10.7125 5.30621 10.7125 5.57772 10.441C5.84924 10.1695 5.84924 9.72858 5.57772 9.45707L5.37355 9.2529H7.86493V11.7465L7.66076 11.5423C7.38925 11.2708 6.94831 11.2708 6.6768 11.5423C6.40529 11.8138 6.40529 12.2547 6.6768 12.5262L8.06694 13.9164C8.33845 14.1879 8.77938 14.1879 9.05089 13.9164L10.441 12.5262C10.7125 12.2547 10.7125 11.8138 10.441 11.5423C10.1695 11.2708 9.72858 11.2708 9.45707 11.5423L9.2529 11.7465V9.25507H11.7465L11.5423 9.45924C11.2708 9.73076 11.2708 10.1717 11.5423 10.4432C11.8138 10.7147 12.2547 10.7147 12.5262 10.4432L13.9164 9.05306C14.1879 8.78155 14.1879 8.34062 13.9164 8.06911L12.5262 6.67897C12.2547 6.40746 11.8138 6.40746 11.5423 6.67897C11.2708 6.95048 11.2708 7.39142 11.5423 7.66293L11.7465 7.8671H9.25507V5.37355L9.45924 5.57772C9.73076 5.84924 10.1717 5.84924 10.4432 5.57772C10.7147 5.30621 10.7147 4.86528 10.4432 4.59377L9.05306 3.20363H9.05089Z" fill="white"/>
+            </svg></button><button class="deletePhoto"><svg xmlns="http://www.w3.org/2000/svg" width="9" height="11" viewBox="0 0 9 11" fill="none">
+            <path d="M2.71607 0.35558C2.82455 0.136607 3.04754 0 3.29063 0H5.70938C5.95246 0 6.17545 0.136607 6.28393 0.35558L6.42857 0.642857H8.35714C8.71272 0.642857 9 0.930134 9 1.28571C9 1.64129 8.71272 1.92857 8.35714 1.92857H0.642857C0.287277 1.92857 0 1.64129 0 1.28571C0 0.930134 0.287277 0.642857 0.642857 0.642857H2.57143L2.71607 0.35558ZM0.642857 2.57143H8.35714V9C8.35714 9.70915 7.78058 10.2857 7.07143 10.2857H1.92857C1.21942 10.2857 0.642857 9.70915 0.642857 9V2.57143ZM2.57143 3.85714C2.39464 3.85714 2.25 4.00179 2.25 4.17857V8.67857C2.25 8.85536 2.39464 9 2.57143 9C2.74821 9 2.89286 8.85536 2.89286 8.67857V4.17857C2.89286 4.00179 2.74821 3.85714 2.57143 3.85714ZM4.5 3.85714C4.32321 3.85714 4.17857 4.00179 4.17857 4.17857V8.67857C4.17857 8.85536 4.32321 9 4.5 9C4.67679 9 4.82143 8.85536 4.82143 8.67857V4.17857C4.82143 4.00179 4.67679 3.85714 4.5 3.85714ZM6.42857 3.85714C6.25179 3.85714 6.10714 4.00179 6.10714 4.17857V8.67857C6.10714 8.85536 6.25179 9 6.42857 9C6.60536 9 6.75 8.85536 6.75 8.67857V4.17857C6.75 4.00179 6.60536 3.85714 6.42857 3.85714Z" fill="white"/>
+          </svg></button>
+      </img>`;
       figCaptionGallery.innerText = "éditer";
 
       figGallery.appendChild(figCaptionGallery);
@@ -49,4 +55,23 @@ export async function genererAffichageTravaux(
     // Ajout de la divGallery à la section Portfolio
     container.appendChild(modal1PhotosWrapper);
   }
+}
+
+/**
+ * Fonction qui gère la récupération et l'affichage des catégories
+ */
+export async function genererCategories() {
+  let select = document.querySelector(".formModal2 select");
+
+let categories = null;
+  categories = await fetch("http://localhost:5678/api/categories").then((categories) => (
+    categories.json()
+  ))
+  if(categories !== null){
+  for (let i = 0; i < categories.length; i++) {
+    let option = document.createElement("option");
+    option.value = categories[i].name;
+    option.innerText = categories[i].name;
+    select.appendChild(option);
+  }}
 }
